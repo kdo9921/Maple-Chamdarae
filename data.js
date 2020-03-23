2605a4008e0d11b585a79f634b63cc024cff8ee4
Kiwi <kdo9921@apteryx.moe>
2020-03-23 10:52:00 +0000

    폰트수정

--- data.js
+++ data.js
@@ -1,11 +1,11 @@
 var data = {
 	level : "244",
-	popular : "379",
+	popular : "383",
 	mureung_floor : "46",
 	mureung_time : "12분 50초",
-	rank_world : "5,521위",
-	rank_world_job : "54위",
-	rank_job : "353위",
+	rank_world : "5,556위",
+	rank_world_job : "55위",
+	rank_job : "354위",
 	union_tier : "마스터 II",
 	union_level : "6081"
 };
