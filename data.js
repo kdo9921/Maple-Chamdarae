e3edec9770942d3d65fd05b7bddf0e687cb57974
키위 <kdo9921@apteryx.moe>
2021-02-06 04:24:42 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,12 +1,12 @@
 var data = {
-	level : "245",
-	popular : "391",
-	guild : "<span>(없음)</span>",
-	mureung_floor : "47",
-	mureung_time : "14분 5초",
-	rank_world : "5,534위",
-	rank_world_job : "54위",
-	rank_job : "337위",
-	union_tier : "마스터 II",
-	union_level : "6082"
+	level : "256",
+	popular : "641",
+	guild : '<a class="text-yellow text-underline" href="https://maple.gg/guild/elysium/%EB%A9%94%EA%B5%AC%EB%B0%8D">메구밍</a>',
+	mureung_floor : "50",
+	mureung_time : "14분 29초",
+	rank_world : "2,993위",
+	rank_world_job : "37위",
+	rank_job : "200위",
+	union_tier : "마스터 IV",
+	union_level : "7048"
 };
